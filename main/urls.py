1a8088226af53a2b43845d2b3b144f983726f889
Randomnation <krammtacular@gmail.com>
2016-07-29 22:51:04 +0800

    updated main app

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,6 @@ from main import views
 
 
 urlpatterns = [
-    url(r'$', views.main_page, name='index'),
+    url(r'^$', views.main_page, name='index'),
+    url(r'^contact/$', views.contact_page, name='contact'),
 ]
--- main/views.py
+++ main/views.py
@@ -5,3 +5,8 @@ from django.template import RequestContext
 def main_page(request):
     css1 = 'active'
     return render_to_response('main/index.html', locals(), context_instance=RequestContext(request))
+
+
+def contact_page(request):
+    css3 = 'active'
+    return render_to_response('main/contact.html', locals(), context_instance=RequestContext(request))
