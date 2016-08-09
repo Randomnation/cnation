b00b6ba3b1f149787f9fc541e03fd2ac80a73094
Randomnation <krammtacular@gmail.com>
2016-08-09 21:35:06 +0800

    Started work on product display

--- cnation/urls.py
+++ cnation/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^main/', include('main.urls', namespace='main')),
+    url(r'^', include('main.urls', namespace='main')),
     url(r'^products/', include('products.urls', namespace='products')),
     url(r'^order/', include('ordering.urls', namespace='order')),
 ]
--- products/views.py
+++ products/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from models import Product
 
 
 def products_page(request):
     css2 = 'active'
+    prod = Product.objects.all()
     return render_to_response('products/products.html', locals(), context_instance=RequestContext(request))
