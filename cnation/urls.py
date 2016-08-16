9bf7f3997f2488308072939f973e69fa9d20d56f
Randomnation <krammtacular@gmail.com>
2016-08-16 21:03:29 +0800

    fixed main urls.py

--- cnation/urls.py
+++ cnation/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('main.urls', namespace='main')),
+    url(r'^main/', include('main.urls', namespace='main')),
     url(r'^products/', include('products.urls', namespace='products')),
     url(r'^order/', include('ordering.urls', namespace='order')),
 ]
